657603cd9b6452168f88c072a935485099f0cbc6
dvir.monajem <dmenajem@gmail.com>
2024-10-10 09:34:03 +0300

    🦄 refactor(app): refactor files
    move db firebase logic to separate file

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const admin = require("firebase-admin");
 
 dotenv.config();
 
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-});
-
-const db = admin.firestore();
+const db = require("./firebase");
 
 const app = express();
 
--- firebase.js
+++ firebase.js
@@ -1,13 +1,14 @@
 const admin = require("firebase-admin");
 
 const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT);
+const credential = admin.credential.cert(serviceAccount);
+const databaseURL = process.env.FIREBASE_DATABASE_URL;
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  credential,
+  databaseURL,
 });
 
 const db = admin.firestore();
 
 module.exports = db;
-
