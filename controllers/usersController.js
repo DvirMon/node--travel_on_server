b2476a4c16d4c31c405a43cf13c2589cd0982dc8
dvir.monajem <dmenajem@gmail.com>
2024-10-22 14:21:08 +0300

    🐞 fix(token): Token
    token is uid instead of jwt

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,33 +1,30 @@
 const usersService = require("../services/usersService");
 
-
 exports.getUserById = async (req, res) => {
-  const { uid } = req?.user;  // UID from the decoded token
+  const { uid } = req?.user; // UID from the decoded token
 
   try {
     // Call the service method to fetch user data
     const user = await usersService.getUserById(uid);
 
-    return res.status(200).json(user);  // Return the user data
+    return res.status(200).json(user); // Return the user data
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 };
 
-
 // Create a new user in Firestore
 exports.createFirestoreUser = async (req, res) => {
-    const { userId, userData } = req.body;
-  
-    if (!userId || !userData) {
-      return res.status(400).json({ error: "UserId and userData are required" });
-    }
-  
-    try {
-      await usersService.createFirestoreUser(userId, userData);
-      res.status(201).json({ message: "User created in Firestore successfully" });
-    } catch (error) {
-      res.status(500).json({ error });
-    }
-  };
-  
+  const { userId, userData } = req.body;
+
+  if (!userId || !userData) {
+    return res.status(400).json({ error: "UserId and userData are required" });
+  }
+
+  try {
+    await usersService.createFirestoreUser(userId, userData);
+    res.status(201).json({ message: "User created in Firestore successfully" });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
--- middleware/verifyUserToken.js
+++ middleware/verifyUserToken.js
@@ -1,24 +1,19 @@
 const { auth } = require("firebase-admin"); // Firebase Admin SDK
-const jwtDecode = require('jwt-decode');
+const jwtDecode = require("jwt-decode");
 
 const verifyUserToken = async (req, res, next) => {
-
   const idToken = req.headers.authorization?.split(" ")[1]; // Extract the token
-  
 
   if (!idToken) {
     return res.status(401).send("Unauthorized: No token provided");
   }
 
-  console.log(idToken)
-
   try {
     // const decodedToken = await auth.verifyIdToken(idToken); // Verify the token
-    const decodedToken = jwtDecode(idToken);
-    console.log(decodedToken)
+    // const decodedToken = jwtDecode(idToken);
+    // console.log(decodedToken)
 
-    
-    req.user = 'decodedToken'; // Attach the decoded user information to the request
+    req.user = { uid: idToken }; // Attach the decoded user information to the request
     next(); // Continue to the next middleware or route handler
   } catch (error) {
     return res.status(403).send("Unauthorized: Invalid token");
